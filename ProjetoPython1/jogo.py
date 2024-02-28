0c03c5e102e58d42c08e43167f9a6eb5fe4b3203
ryangabriel27 <ryanjulio56@gmail.com>
2024-02-28 19:42:51 -0300

    28/02

--- ProjetoPython1/jogo.py
+++ ProjetoPython1/jogo.py
@@ -4,20 +4,137 @@ import random
 print("Bem-Vindo ao Quem quer ser um milionario!")
 escolha = input("Deseja iniciar? \n 1: Sim \n 2: Não")
 
-if(int(escolha) == 1):
+if int(escolha) == 1:
     aberto = True
 else:
     aberto = False
 
-while(aberto):
-    id_pergunta = random.randint(0, 1)
+while aberto:
+    id_pergunta = random.randint(-1, 7)
     print("Vamos começar!!!")
     print("--===X 1º Bloco: Fáceis X===--")
-    print("1ª Pergunta - "+perguntas.banco_perguntas_facil[id_pergunta]['pergunta'])
-    print(perguntas.getOpcoesFacil(id_pergunta))
-    palpite = input()
-    if(palpite == perguntas.banco_perguntas_facil[id_pergunta]['resposta_correta']):
+    primeira = perguntas.getPerguntaFacil(int(id_pergunta))
+    print(f"1ª Pergunta - {primeira}")
+    print(perguntas.getOpcoesFacil(int(id_pergunta)))
+    palpite = input("")
+    if palpite == perguntas.getRespostaFacil(int(id_pergunta)):
         print("Parabéns, voce acertou!!")
     else:
         print("ERROUU!, tente outra vez")
         aberto = False
+
+    id_pergunta2 = random.randint(-1, 7)
+    while id_pergunta2 == id_pergunta:
+        id_pergunta2 = random.randint(-1, 7)
+
+    print(f"2ª Pergunta - {perguntas.getPerguntaFacil(int(id_pergunta2))}")
+    print(perguntas.getOpcoesFacil(int(id_pergunta2)))
+    palpite = input("")
+    if palpite == perguntas.getRespostaFacil(int(id_pergunta2)):
+        print("Parabéns, você acertou!!")
+    else:
+        print("ERROUU!, tente outra vez!")
+        aberto = False
+
+    id_pergunta3 = random.randint(-1, 7)
+    while id_pergunta3 == id_pergunta or id_pergunta3 == id_pergunta2:
+        id_pergunta3 = random.randint(-1, 7)
+
+    print(f"3ª Pergunta - {perguntas.getPerguntaFacil(int(id_pergunta3))}")
+    print(perguntas.getOpcoesFacil(int(id_pergunta3)))
+    palpite = input("")
+    if palpite == perguntas.getRespostaFacil(int(id_pergunta3)):
+        print("Parabéns, você acertou!!")
+    else:
+        print("ERROUU!, tente outra vez!")
+        aberto = False
+
+    id_pergunta4 = random.randint(-1, 7)
+    while (
+        id_pergunta4 == id_pergunta
+        or id_pergunta4 == id_pergunta2
+        or id_pergunta4 == id_pergunta3
+    ):
+        id_pergunta4 = random.randint(-1, 7)
+
+    print(f"4ª Pergunta - {perguntas.getPerguntaFacil(int(id_pergunta4))}")
+    print(perguntas.getOpcoesFacil(int(id_pergunta4)))
+    palpite = input("")
+    if palpite == perguntas.getRespostaFacil(int(id_pergunta4)):
+        print("Parabéns, você acertou!!")
+    else:
+        print("ERROUU!, tente outra vez!")
+        aberto = False
+
+    id_pergunta5 = random.randint(-1, 7)
+    while (
+        id_pergunta5 == id_pergunta
+        or id_pergunta5 == id_pergunta2
+        or id_pergunta5 == id_pergunta3
+        or id_pergunta5 == id_pergunta4
+    ):
+        id_pergunta5 = random.randint(-1, 7)
+
+    print(f"5ª Pergunta - {perguntas.getPerguntaFacil(int(id_pergunta5))}")
+    print(perguntas.getOpcoesFacil(int(id_pergunta5)))
+    palpite = input("")
+    if palpite == perguntas.getRespostaFacil(int(id_pergunta5)):
+        print("Parabéns, você acertou!!")
+    else:
+        print("ERROUU!, tente outra vez!")
+        aberto = False
+    
+    print("===============X=============")
+    print(
+        "Parabéns você concluiu o 1º Bloco, ganhando 1.000 reais. Agora você deve escolher parar e sair com esse dinheiro ou ficar e ter chance de ganhar 1 milhão"
+    )
+    escolha = input("1: Sair com 1000 reais \n 2: Continuar \n")
+    if escolha == 2:
+        print("Ok, você escolheu continuar então prosseguimos")
+        print("--===X 2º Bloco: Médias X===--")
+        print(f"1ª Pergunta - {perguntas.getPerguntaMedia(int(id_pergunta))}")
+        print(perguntas.getOpcoesMedia(int(id_pergunta)))
+        palpite = input("")
+        if palpite == perguntas.getRespostaMedia(int(id_pergunta)):
+            print("Parabéns, voce acertou!!")
+        else:
+            print("ERROUU!, tente outra vez")
+            aberto = False
+        
+        print(f"2ª Pergunta - {perguntas.getPerguntaMedia(int(id_pergunta2))}")
+        print(perguntas.getOpcoesMedia(int(id_pergunta2)))
+        palpite = input("")
+        if palpite == perguntas.getRespostaMedia(int(id_pergunta2)):
+            print("Parabéns, você acertou!!")
+        else:
+            print("ERROUU!, tente outra vez!")
+            aberto = False
+
+        print(f"3ª Pergunta - {perguntas.getPerguntaMedia(int(id_pergunta3))}")
+        print(perguntas.getOpcoesMedia(int(id_pergunta3)))
+        palpite = input("")
+        if palpite == perguntas.getRespostaMedia(int(id_pergunta3)):
+            print("Parabéns, você acertou!!")
+        else:
+            print("ERROUU!, tente outra vez!")
+            aberto = False
+
+        print(f"4ª Pergunta - {perguntas.getPerguntaMedia(int(id_pergunta4))}")
+        print(perguntas.getOpcoesMedia(int(id_pergunta4)))
+        palpite = input("")
+        if palpite == perguntas.getRespostaMedia(int(id_pergunta4)):
+            print("Parabéns, você acertou!!")
+        else:
+            print("ERROUU!, tente outra vez!")
+            aberto = False
+
+        print(f"5ª Pergunta - {perguntas.getPerguntaMedia(int(id_pergunta5))}")
+        print(perguntas.getOpcoesMedia(int(id_pergunta5)))
+        palpite = input("")
+        if palpite == perguntas.getRespostaMedia(int(id_pergunta5)):
+            print("Parabéns, você acertou!!")
+        else:
+            print("ERROUU!, tente outra vez!")
+            aberto = False
+
+        print("===============X=============")
--- ProjetoPython1/perguntas.py
+++ ProjetoPython1/perguntas.py
@@ -2,18 +2,43 @@
 banco_perguntas_facil = [
     {
         "pergunta": "Qual é a capital do Brasil?",
-        "opcoes": [
-            "A) São Paulo",
-            "B) Rio de Janeiro",
-            "C) Brasília",
-            "D) Belo Horizonte",
-        ],
-        "resposta_correta": "C) Brasília",
+        "opcoes": ["A) São Paulo","B) Rio de Janeiro","C) Brasília","D) Belo Horizonte"],
+        "resposta_correta": "C",
     },
     {
         "pergunta": "Qual é a cor do céu em um dia claro?",
         "opcoes": ["A) Azul", "B) Verde", "C) Vermelho", "D) Amarelo"],
-        "resposta_correta": "A) Azul",
+        "resposta_correta": "A",
+    },
+    {
+        'pergunta': 'Quantos lados tem um triângulo?',
+        'opcoes': ['A) 3', 'B) 4', 'C) 5', 'D) 6'],
+        'resposta_correta': 'A'
+    },
+    {
+        'pergunta': 'Quem pintou a Mona Lisa?',
+        'opcoes': ['A) Leonardo da Vinci', 'B) Michelangelo', 'C) Pablo Picasso', 'D) Vincent van Gogh'],
+        'resposta_correta': 'A'
+    },
+    {
+        'pergunta': 'Quem foi o primeiro presidente do Brasil?',
+        'opcoes': ['A) Getúlio Vargas', 'B) Juscelino Kubitschek', 'C) Marechal Deodoro da Fonseca', 'D) Tancredo Neves'],
+        'resposta_correta': 'C'
+    },
+    {
+        'pergunta': 'Qual é o maior oceano do mundo?',
+        'opcoes': ['A) Atlântico', 'B) Índico', 'C) Ártico', 'D) Pacífico'],
+        'resposta_correta': 'D'
+    },
+     {
+        'pergunta': 'Qual é a capital da França?',
+        'opcoes': ['A) Madri', 'B) Roma', 'C) Paris', 'D) Berlim'],
+        'resposta_correta': 'C'
+    },
+    {
+        'pergunta': 'Quem é conhecido como "Rei do Pop"?',
+        'opcoes': ['A) Elvis Presley', 'B) Michael Jackson', 'C) Madonna', 'D) Prince'],
+        'resposta_correta': 'B'
     },
     # Adicione mais perguntas fáceis conforme necessário
 ]
@@ -21,19 +46,45 @@ banco_perguntas_facil = [
 banco_perguntas_media = [
     {
         "pergunta": "Quem foi o primeiro presidente dos Estados Unidos?",
-        "opcoes": [
-            "A) Abraham Lincoln",
-            "B) George Washington",
-            "C) Thomas Jefferson",
-            "D) John Adams",
-        ],
-        "resposta_correta": "B) George Washington",
+        "opcoes": ["A) Abraham Lincoln","B) George Washington","C) Thomas Jefferson","D) John Adams",],
+        "resposta_correta": "B",
     },
     {
         "pergunta": "Qual é o símbolo químico para o ouro?",
         "opcoes": ["A) Au", "B) Ag", "C) Fe", "D) Hg"],
-        "resposta_correta": "A) Au",
+        "resposta_correta": "A",
     },
+    {
+        'pergunta': 'Em que ano ocorreu a Revolução Industrial?',
+        'opcoes': ['A) 1750', 'B) 1800', 'C) 1850', 'D) 1900'],
+        'resposta_correta': 'C'
+    },
+    {
+        'pergunta': 'Quem foi o primeiro astronauta a caminhar na Lua?',
+        'opcoes': ['A) Neil Armstrong', 'B) Buzz Aldrin', 'C) Yuri Gagarin', 'D) John Glenn'],
+        'resposta_correta': 'A'
+    },
+    {
+        'pergunta': 'Quem foi o autor de "Cem Anos de Solidão"?',
+        'opcoes': ['A) Gabriel García Márquez', 'B) Julio Cortázar', 'C) Isabel Allende', 'D) Mario Vargas Llosa'],
+        'resposta_correta': 'A'
+    },
+    {
+        'pergunta': 'Qual é a fórmula química da água?',
+        'opcoes': ['A) CO2', 'B) H2O', 'C) NaCl', 'D) O2'],
+        'resposta_correta': 'B'
+    },
+    {
+        'pergunta': 'Quem é considerado o "pai da computação"?',
+        'opcoes': ['A) Alan Turing', 'B) Bill Gates', 'C) Steve Jobs', 'D) Mark Zuckerberg'],
+        'resposta_correta': 'A'
+    },
+    {
+        'pergunta': 'Qual é a unidade de medida de temperatura no sistema internacional?',
+        'opcoes': ['A) Kelvin', 'B) Celsius', 'C) Fahrenheit', 'D) Rankine'],
+        'resposta_correta': 'B'
+    },
+    
     # Adicione mais perguntas médias conforme necessário
 ]
 
@@ -46,7 +97,7 @@ banco_perguntas_dificil = [
             "C) 450.000 km/s",
             "D) 600.000 km/s",
         ],
-        "resposta_correta": "A) 300.000 km/s",
+        "resposta_correta": "A",
     },
     {
         "pergunta": "Quem descobriu a penicilina?",
@@ -56,13 +107,38 @@ banco_perguntas_dificil = [
             "C) Louis Pasteur",
             "D) Jonas Salk",
         ],
-        "resposta_correta": "A) Alexander Fleming",
+        "resposta_correta": "A",
     },
     # Adicione mais perguntas difíceis conforme necessário
 ]
 
 def getOpcoesFacil(index):
-    opcoes = banco_perguntas_facil[index]['opcoes']
+    opcoes = banco_perguntas_facil[int(index)]['opcoes']
     for opcao in opcoes:
         print(opcao)
+
+def getPerguntaFacil(index):
+    pergunta = banco_perguntas_facil[int(index)]['pergunta']
+    return pergunta
+
+def getRespostaFacil(index):
+    resposta = banco_perguntas_facil[int(index)]['resposta_correta']
+    return resposta
+
+def getOpcoesMedia(index):
+    opcoes = banco_perguntas_media[int(index)]['opcoes']
+    for opcao in opcoes:
+        print(opcao)
+
+def getPerguntaMedia(index):
+    pergunta = banco_perguntas_media[int(index)]['pergunta']
+    return pergunta
+
+def getRespostaMedia(index):
+    resposta = banco_perguntas_media[int(index)]['resposta_correta']
+    return resposta
+
+# def gerarNumerosAleatorios():
+
+    
     
